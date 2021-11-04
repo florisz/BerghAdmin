801ff4828f8f52b42fb8d8f8c44d588be163d96c
Floris Zwarteveen <floris.zwarteveen@itandcare.nl>
2021-11-04 15:38:12 +0100

    Deployment build aangepast voor een win-64 build

--- BuildBihzRelease.ps1
+++ BuildBihzRelease.ps1
@@ -1,3 +1,3 @@
 #dotnet publish --runtime win-x64 --configuration Release -verbosity:normal --self-contained true bihz-kantoorportaal-proj/bihz-kantoorportaal.csproj
-# publish for linux
-dotnet publish -c Release --self-contained -r linux-x64
+# publish for win-x64
+dotnet publish BerghAdmin/BerghAdmin.csproj --configuration Release --self-contained true --runtime win-x64 -verbosity:normal
