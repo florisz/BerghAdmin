3009445828e6fcddae73347c176820b24d2dcd35
Floris Zwarteveen <floris.zwarteveen@itandcare.nl>
2022-04-21 13:24:25 +0200

    fixed bug in unit test on build server

--- Deployment/provision.azcli.ps1
+++ Deployment/provision.azcli.ps1
@@ -1,38 +1,49 @@
-$rg = "bergh-uat"
+$rg = "BerghTest"
 $location = "westeurope"
+
+$plan = "bergh-test-plan"
+$webapp = "bergh-test-bergh-admin-webapp"
+$webmonitor = "bergh-test-bergh-monitor-webapp"
+$storageaccount = "bergh-test-storage-account"
+$workspace = "bergh-test-workspace"
+$appinsights = "bergh-test-appinsights"
+$functionplan = "bergh-test-functionplan"
+$functionappkentaa = "bergh-test-functionapp-kentaa"
+
 az group create `
     --name $rg `
     --location $location
 
 az appservice plan create `
     --resource-group $rg `
-    --name bergh-uat-plan `
+    --name $plan `
     --location $location `
     --sku B1 `
     --is-linux `
     --number-of-workers 1
 
+#### Web Apps
 az webapp create `
-    --name berghadmin-uat `
+    --name $webapp `
     --resource-group $rg `
-    --plan bergh-uat-plan `
+    --plan $plan `
     --runtime '"dotnetcore|6.0"' 
 
 az webapp create `
-    --name bergh-monitor-uat `
+    --name $webmonitor `
     --resource-group $rg `
-    --plan bergh-uat-plan `
+    --plan $plan `
     --runtime '"dotnetcore|6.0"' 
 
-#### Function App
+#### Function Apps
 az storage account create `
-    --name berghuatstorage `
+    --name $storageaccount `
     --resource-group $rg `
     --location $location `
     --sku Standard_LRS	
 
 az monitor log-analytics workspace create `
-    --workspace-name bergh-uat-workspace `
+    --workspace-name $workspace `
     --resource-group $rg `
     --location $location `
     --retention-time 30
@@ -40,11 +51,11 @@ az monitor log-analytics workspace create `
 az monitor app-insights component create `
     --resource-group $rg `
     --location $location `
-    --app bergh-uat-appinsights
+    --app $appinsights
     # --app-id $appid
 
 az functionapp plan create `
-    --name bergh-uat-functions `
+    --name $functionplan `
     --resource-group $rg `
     --location $location `
     --sku B1 `
@@ -52,16 +63,16 @@ az functionapp plan create `
     --number-of-workers 1
 
 az functionapp create `
-    --name bergh-uat-kentaa `
+    --name $functionappkentaa `
     --resource-group $rg `
     --os-type Linux `
     --functions-version 4 `
     --runtime dotnet `
     --runtime-version 6 `
-    --storage-account berghuatstorage `
+    --storage-account $storageaccount `
     --plan bergh-uat-functions
 
 az functionapp config appsettings set `
-    --name bergh-uat-kentaa `
+    --name $functionappkentaa `
     --resource-group $rg `
     --settings "?????????????AzureWebJobsStorage=$storageConnectionString"
