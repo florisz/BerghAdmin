7e346cbf8d0245a668f8dd0c9fd217d6983407b0
Reinald Baart <r.baart@itandcare.nl>
2022-06-11 18:51:44 +0200

    Dialog to edit MailAddress

---
+++ BerghAdmin/Pages/EditMailAddressDialog.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BerghAdmin.Pages
+{
+    public partial class EditMailAddressDialog
+    {
+
+        public MailAddress Address { get; set; } = new(string.Empty, string.Empty);
+
+        [Parameter]
+        public bool ShowDialog { get; set; } = false;
+        [Parameter]
+        public List<MailAddress> MailAddresses { get; set; } = new();
+
+        public void DialogOpen()
+        {
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public void SaveMailAddress()
+        {
+            CloseDialog();
+            StateHasChanged();
+        }
+        public void DeleteMailAddress()
+        {
+            MailAddresses.Remove(Address);
+            StateHasChanged();
+        }
+
+        public void CloseDialog()
+        {
+            ShowDialog = false;
+        }
+
+        public void OnOverlayclick()
+        {
+            CloseDialog();
+        }
+
+    }
+}
