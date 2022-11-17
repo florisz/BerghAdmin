d9737385f96cb272970fe9054f39f90ff21d1e4e
Richard de Zwart <richard.dezwart@deptagency.com>
2022-11-17 16:27:01 +0100

    UI Test to login and navigate to fietsers

--- BerghAdmin.UITests/LoginTests.cs
+++ BerghAdmin.UITests/LoginTests.cs
@@ -1,25 +1,43 @@
-﻿using Microsoft.Playwright.NUnit;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
 using NUnit.Framework;
 
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace BerghAdmin.UITests;
 
 [TestFixture]
-public class LoginTests:PageTest
+public class LoginTests : PageTest
 {
-    public LoginTests():base()
+    public LoginTests() : base()
     {
 
     }
+    
     [Test]
-
     public async Task LoginPageLoads()
     {
         var page = await Page.GotoAsync("https://bergh-test-admin-webapp.azurewebsites.net/Identity/Account/Login?ReturnUrl=%2F");
-        
+
         Assert.NotNull(page);
         Assert.IsTrue(page!.Ok);
         Assert.AreEqual((int)HttpStatusCode.OK, page!.Status);
+    }
+
+    [Test]
+    public async Task FietsersPageShowsData()
+    {
+        await Page.GotoAsync("https://bergh-test-admin-webapp.azurewebsites.net/Identity/Account/Login?ReturnUrl=%2F");
+        await Page.GetByLabel("E-mail").ClickAsync();
+        await Page.GetByLabel("E-mail").FillAsync("admin@berghinhetzadel.nl");
+        await Page.GetByLabel("E-mail").PressAsync("Tab");
+        await Page.GetByLabel("Wachtwoord").FillAsync("Qwerty@123");
+        await Page.GetByLabel("Wachtwoord").PressAsync("Enter");
+        await Page.GetByRole(AriaRole.Button, new() { NameString = "Beheer Fietsers" }).ClickAsync();
+
+        // Find title
+        var title = Page.GetByText("Fietsers", new() { Exact = true });
     }
 }
